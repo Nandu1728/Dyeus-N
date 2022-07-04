21e433d164d1f5d190389d2de286cb1541f4616a
Nandu1728 <ugs19007_it.nandini@cbit.ac.in>
2022-07-04 09:00:46 +0530

    test

---
+++ src/components/Banner1.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Banner1() {
+  return (
+    <><div className="Banner1 text-center pb-4">
+        <h1 classname="sub-text4 pt-5 pe-3 ps-3">What makes us different?</h1>
+        <p className='para4'>Reimagining and rewiring self-care</p>
+        <p classname='para5 ps-5 pe-5'>You care about conscious consumption,and so do we.
+        <br></br>
+        <br></br>
+        We collobrate with researches and doctors to
+        <br></br>
+        <br></br>
+        <ul>
+            <li>Help you get granular about your health and self-care with personalised insights,and</li>
+            <br></br>
+            <li>Equip with simple,sustainable products than get the job done</li>
+        </ul>
+        <br></br>
+
+    No to-the-moon-and-back claims in this neck of the woods.
+    <br></br> 
+    <br></br>
+
+    We're just solving real problems for real people here.</p>
+    </div></>
+  )
+}
+
+export default Banner1
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Banner from '../components/Banner'
+import Banner1 from '../components/Banner1'
+import Banner2 from '../components/Banner2'
+import Banner3 from '../components/Banner3'
+function Home() {
+  return (
+    <>
+    <Navbar />
+    <Banner />
+    <Banner1 />
+    <Banner2 />
+    <Banner3 />
+    </>
+  )
+}
+
+export default Home
