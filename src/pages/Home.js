770520770778f6acc18e0343e2ccc784bdb5d1db
Gopasi Nandini <105743114+Nandu1728@users.noreply.github.com>
2022-07-04 22:13:16 +0530

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,11 @@ import Banner from '../components/Banner'
 import Banner1 from '../components/Banner1'
 import Banner2 from '../components/Banner2'
 import Banner3 from '../components/Banner3'
+import Banner4 from '../components/Banner4'
+import Banner5 from '../components/Banner5'
+import Banner6 from '../components/Banner6'
+import Banner7 from '../components/Banner7'
+import Footer from '../components/Footer'
 function Home() {
   return (
     <>
@@ -12,6 +17,11 @@ function Home() {
     <Banner1 />
     <Banner2 />
     <Banner3 />
+    <Banner4 />
+    <Banner5 />
+    <Banner6 />
+    <Banner7 />
+    <Footer />
     </>
   )
 }
